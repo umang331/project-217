bb31d6ccafab2b7276eec2ec0942a74bcb904848
Umang balicha <54236975+umang331@users.noreply.github.com>
2022-11-10 14:22:58 +0530

    Add files via upload

---
+++ project-217/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+io.on("connection", (socket) => {
+  socket.on("message", (message) => {
+    io.emit("createMessage", message);
+  });
+});
+
+server.listen(3030, () => {
+  console.log("Running on http://localhost:3030");
+});
